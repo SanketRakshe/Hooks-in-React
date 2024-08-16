d49d0e904ac79efe96c70c392ab800c3cc7b0eb9
sanketrakshe <sanketrakshe07@gmail.com>
2024-08-16 15:52:58 +0530

    Added: useEffect hook implementation

--- src/App.jsx
+++ src/App.jsx
@@ -25,30 +25,76 @@ import React, { useState, useEffect } from 'react';
 // }
 
 
-// useEffect hook example for data fetching side effect
-function App() {
+
+
+// useEffect hook example for data fetching side effect --------------------
+
+// function App() {
   
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(null);
+//   const [data, setData] = useState(null);
+//   const [loading, setLoading] = useState(null);
 
-  useEffect(() => {
-      fetch('https://jsonplaceholder.typicode.com/posts/1')
-      .then((response) => response.json())
-      .then((json) => {
-        setData(json);
-        setLoading(false);
-      })
-  }, []);
+//   useEffect(() => {
+//       fetch('https://jsonplaceholder.typicode.com/posts/1')
+//       .then((response) => response.json())
+//       .then((json) => {
+//         setData(json);
+//         setLoading(false);
+//       })
+//   }, []);
 
 
-  return (
-      <div>
+//   return (
+//       <div>
 
-        <h1>data.title</h1>
-        <div>data.body</div>
+//         <h1>data.title</h1>
+//         <div>data.body</div>
 
-      </div>
-  )
-};
+//       </div>
+//   )
+// };
+
+
+
+// function App() {
+
+//   const [todos, setTodos] = useState([]);
+
+//   useEffect(()=> {
+//       fetch('https://sum-server.100xdevs.com/todos')
+//       .then(async function(response) {
+//           const json = await response.json();
+//           setTodos(json.todos);
+//       })
+//   }, [])
+
+//   return (
+//       <div>
+//           {/* {todos.map(todo => <Todo key={todo.id} title={todo.title} description = {todo.description} />)} */}
+
+//           {
+//               todos.map( (todo) => {
+//                 return (
+//                   <Todo key={todo.id} title={todo.title} description={ todo.description} />
+//                 )
+//               })
+//           }
+
+//       </div>
+//   )
+// }
+
+// function Todo({title, description}) {
+//   return (
+//     <>
+//       <h1>{title}</h1>
+//       <p>{description}</p>
+    
+//     </>
+//   )
+// }
+
+
+
 
 export default App
