552abe6c9f384bf80ddb4ac1f1b73877c18e3bb4
sanketrakshe <sanketrakshe07@gmail.com>
2024-08-16 15:32:07 +0530

    Added: useEffect hook implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,54 @@
 import './App.css'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 // Example for useState hook --------------------------------
+// function App() {
+
+//   const [count, setCount] = useState(0);
+
+//   function increment() {
+//       setCount(count+1);
+//   }
+
+//   function Decrement() {
+//       setCount(count-1);
+//   }
+
+//   return (
+//       <div>
+//           <div>{count}</div>
+//           <button onClick={increment}>Increment</button>
+//           <button onClick={Decrement}>Decrement</button>
+//       </div>
+//   )
+// }
+
+
+// useEffect hook example for data fetching side effect
 function App() {
+  
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(null);
 
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);
+        setLoading(false);
+      })
+  }, []);
 
-  function increment() {
-      setCount(count+1);
-  }
-
-  function Decrement() {
-      setCount(count-1);
-  }
 
   return (
       <div>
-          <div>{count}</div>
-          <button onClick={increment}>Increment</button>
-          <button onClick={Decrement}>Decrement</button>
+
+        <h1>data.title</h1>
+        <div>data.body</div>
+
       </div>
   )
-}
+};
 
 export default App
